4c7676cbe6f274e82bb07b6370f0bdfe1af0867b
Brandon <bb1990@my.yorku.ca>
2022-07-08 15:30:30 -0400

    Added a method to insert a node at the beginning of the linkedlist and changed variable names within the create_node method

--- list.c
+++ list.c
@@ -14,26 +14,43 @@ struct node * create_linked_list()
 {
 
     // Creates the head with date
-    node * new_head = create_node();
-    new_head->data = 45;
-    new_head->link = NULL;
+    node * head = create_node();
+    head->data = 45;
+    head->link = NULL;
 
     // Creates the next node and links this with the initial node
     node * next_node = create_node();
     next_node->data = 5;
     next_node->link = NULL;
-    new_head->link = next_node;
+    head->link = next_node;
 
     // Reuse the node to create a third node
     next_node = create_node();
     next_node->data = 6;
     next_node->link = NULL;
-    new_head->link->link = next_node;
+    head->link->link = next_node;
 
     return new_head;
 }
 
+
+//
+// Insert node at the beginning of the list
+//
+
+struct node * insert_node_beginning(struct node *head)
+{
+    struct node * new_head = create_node()
+    new_head->data = 55;
+    new_head->link = head;
+    head = new_head;
+    return head;
+}
+
+
+//
 // Counts the number of linked_list nodes
+//
 void count_nodes(struct node * head)
 {
 int count;
